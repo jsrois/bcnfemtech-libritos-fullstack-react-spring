255ac29946848615910faecd7bdc0279801d0978
Javi <javi@jsrois.net>
2021-07-20 19:54:26 +0200

    Call the backend to get books

--- web-client/src/js/components/app.jsx
+++ web-client/src/js/components/app.jsx
@@ -3,38 +3,27 @@ import {NavigationBar} from "./navigationBar";
 import {BrowserRouter as Router, Switch, Route} from "react-router-dom"
 import {About} from "./about";
 import {Catalog} from "./catalog";
+import {useEffect, useState} from "react";
 
 
 export const App = () => {
 
-    const bookList = [
-        {
-            title: "The lord of the Rings",
-            author: "J.R.R Tolkien",
-            genre: "Fantasy"
-        },
-        {
-            title: "Harry Potter and the React Application",
-            author: "J.K. Rowling",
-            genre: "Fantasy"
-        },
-        {
-            title: "Un mago de Terramar",
-            author: "Ursula K. Le Guin",
-            genre: "Fantasy"
-        }
-    ]
+    const [books, setBooks] = useState([])
 
-
+    useEffect(() => {
+        fetch("/books")
+            .then(response => response.json())
+            .then(setBooks)
+    })
 
     return <Router>
         <NavigationBar/>
         <Switch>
             <Route path="/catalog">
-                <Catalog books={bookList} />
+                <Catalog books={books}/>
             </Route>
             <Route path="/about">
-                <About />
+                <About/>
             </Route>
         </Switch>
     </Router>
