fa74e96695e0bb752c9f53f3864cf808d03cb85d
Javi <javi@jsrois.net>
2021-07-20 18:35:33 +0200

    Rename packages

--- src/main/java/org/libritos/libritosapp/ExampleApplication.java
+++ src/main/java/org/libritos/libritosapp/ExampleApplication.java
@@ -1,4 +1,4 @@
-package org.example.exampleapp;
+package org.libritos.libritosapp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
